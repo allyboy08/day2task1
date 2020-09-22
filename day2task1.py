30a8e2205a4e21a363f162d5c1f0c483ff7ee870
allyboy08 <alexsassman65@gmail.com>
2020-09-22 15:31:59 +0200

    new task1 commit

---
+++ day2task1.py
@@ -0,0 +1,23 @@
+a= "1) a list is a collection which is ordered and changeable. Allows duplicate members, a dictionary  is a collection which is unordered, changeable and indexed. No duplicate members."
+print(a)
+
+b= "2) string.title()"
+print(b)
+
+c= "3) int, str, float, complex, list, tuple, set, dictionary"
+print(c)
+
+d= "4) Hello World!"
+print(d)
+
+e= "5) llo "
+print(e)
+
+f= "6) llo World!"
+print(f)
+
+g= "7) print(syntax.key())"
+print(g)
+
+h= "8) print(len(syntax))"
+print(h)
